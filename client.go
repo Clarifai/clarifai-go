6101dc984a596f1216b16b91c5cbf50ca3c73889
Sam Couch <samcouch91@gmail.com>
2015-09-28 16:24:57 -0400

    some getters

--- client.go
+++ client.go
@@ -14,8 +14,25 @@ type ClarifaiClient struct {
 	accessToken  string
 	apiHost      string
 	apiPort      string
+	tokenRetries integer
 }
 
+// Initialize a new client object
 func initClient(clientID, clientSecret, accessToken string) *ClarifaiClient {
 	return &ClarifaiClient{clientID, clientSecret, accessToken, "api.clarifai.com", "443"}
+}
+
+// clienID getter
+func (self *ClarifaiClient) getClientID() string {
+	return self.clientID
+}
+
+// clientSecret getter
+func (self *ClarifaiClient) getClientSecret() string {
+	return self.clientSecret
+}
+
+// accessToken getter
+func (self *ClarifaiClient) getAccessToken() string {
+	return self.accessToken
 }
