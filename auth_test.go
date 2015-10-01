da85a1c426c4d89235649a8cb04f6af2f5f9bbee
Sam Couch <samcouch91@gmail.com>
2015-09-30 21:12:40 -0400

    make sure the token is saved

--- auth_test.go
+++ auth_test.go
@@ -23,7 +23,7 @@ func TestRequestAccessToken(t *testing.T) {
 	mux.HandleFunc("/v1/token", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintln(w, `{"access_token":"U5j3rECLLZ86pWRjK35g489QJ4zrQI","expires_in":36000,"scope": "api_access", "token_type": "Bearer"}`)
+		fmt.Fprintln(w, `{"access_token":"1234567890abcdefg","expires_in":36000,"scope": "api_access", "token_type": "Bearer"}`)
 	})
 
 	_, err := client.requestAccessToken()
@@ -43,13 +43,34 @@ func TestRequestAccessTokenFail(t *testing.T) {
 
 	mux.HandleFunc("/v1/token", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(400)
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintln(w, `{"access_token":"U5j3rECLLZ86pWRjK35g489QJ4zrQI","expires_in":36000,"scope": "api_access", "token_type": "Bearer"}`)
 	})
 
 	token, _ := client.requestAccessToken()
 
 	if token != nil {
 		t.Errorf("requestAccessToken() should return an err with an invalid request: %v", token)
+	}
+}
+
+func TestAccessTokenIsSaved(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	client := NewClient(clientID, clientSecret)
+	client.APIHost = server.URL
+
+	defer server.Close()
+
+	mux.HandleFunc("/v1/token", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, `{"access_token":"1234567890abcdefg","expires_in":36000,"scope": "api_access", "token_type": "Bearer"}`)
+	})
+
+	_, err := client.requestAccessToken()
+
+	if err != nil {
+		if client.AccessToken != "1234567890abcdefg" {
+			t.Errorf("requestAccessToken() should store the access token. Expected: 1234567890abcdefg, Got: %v", client.AccessToken)
+		}
 	}
 }
